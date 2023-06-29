3327c257d30f2c7ca9287be6f7943d766dc9ebf1
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2023-06-29 11:36:39 +0300

    make logout for auth-users only

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as controller from "../controllers/auth";
+import { authMiddleware } from "../middlewares/auth";
 
 const router = Router();
 
 router.post("/signup", controller.signup);
 router.post("/login", controller.login);
-router.post("/logout", controller.logout);
+router.post("/logout", authMiddleware, controller.logout);
 
 export default router;
