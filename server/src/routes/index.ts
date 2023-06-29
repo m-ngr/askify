b56d19164ca7b6fd2feb9cc4d2fa608386c91c97
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2023-06-29 16:05:26 +0300

    Category endpoint implemented

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth";
+import categoryRoutes from "./category";
 
 const router = Router();
 
 router.use(authRoutes);
+router.use("/category", categoryRoutes);
 
 export default router;
