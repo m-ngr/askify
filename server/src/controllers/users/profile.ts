0d1329a6c720d926064a3b8bb3a6a5435a485369
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2023-07-08 06:41:43 +0300

    need testing

---
+++ server/src/controllers/users/profile.ts
@@ -0,0 +1,184 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../../models/User";
+import { isValidObjectId } from "mongoose";
+import Question from "../../models/Question";
+import { isValidRegex, mongooseErrors } from "../../utils/errors";
+
+export async function searchUsers(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  /**
+   * Need Testing
+   */
+  try {
+    const query = req.query.q as string;
+    const page = Math.max(1, parseInt(req.query.page as string) || 1);
+    const limit = Math.max(
+      1,
+      Math.min(parseInt(req.query.limit as string) || 10, 100)
+    );
+
+    const filter: any = {};
+    const sortOption: any = {};
+
+    if (query) {
+      if ("regex" in req.query) {
+        if (!isValidRegex(query)) {
+          return res.status(400).json({ error: "Invalid regex query" });
+        }
+        filter.$or = [
+          { firstName: { $regex: query, $options: "i" } },
+          { lastName: { $regex: query, $options: "i" } },
+          { username: { $regex: query, $options: "i" } },
+          { bio: { $regex: query, $options: "i" } },
+        ];
+      } else {
+        filter.$text = { $search: query };
+        sortOption.score = { $meta: "textScore" };
+      }
+    }
+
+    const users = await User.find(filter, sortOption)
+      .sort({ ...sortOption, _id: 1 })
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.json({ page, users });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getProfile(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  /**
+   * change the fields of the returned object (no email)
+   */
+  try {
+    const handle = req.params.handle;
+
+    const user = await (isValidObjectId(handle)
+      ? User.findById(handle)
+      : User.findOne({ username: handle }));
+
+    if (!user) {
+      return res.status(404).json({ error: "user not found" });
+    }
+
+    res.json(user.publicInfo());
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getAnswers(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  /**
+   * Need Testing
+   */
+  try {
+    const query = req.query.q as string;
+    const category = req.query.cat as string;
+    const sort = String(req.query.sort).toLowerCase();
+    const page = Math.max(1, parseInt(req.query.page as string) || 1);
+    const limit = Math.max(
+      1,
+      Math.min(parseInt(req.query.limit as string) || 10, 100)
+    );
+
+    const handle = req.params.handle;
+    const toUser = isValidObjectId(handle)
+      ? handle
+      : (await User.findOne({ username: handle }, { id: 1 }))?.id;
+
+    if (!toUser) {
+      return res.status(400).json({ error: "Invalid user handle" });
+    }
+
+    const filter: any = { toUser, answer: { $ne: "" } };
+
+    if (category) filter.category = category;
+
+    if (query) {
+      if ("regex" in req.query) {
+        if (!isValidRegex(query)) {
+          return res.status(400).json({ error: "Invalid regex query" });
+        }
+        filter.$or = [
+          { question: { $regex: query, $options: "i" } },
+          { answer: { $regex: query, $options: "i" } },
+        ];
+      } else {
+        filter.$text = { $search: query };
+      }
+    }
+
+    const questions = await Question.find(filter)
+      .sort({ answeredAt: sort === "oldest" ? 1 : -1 })
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.json({ page, questions });
+  } catch (error) {
+    next(error);
+  }
+}
+
+// ============================================================================================
+
+export async function askUser(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { question, isAnonymous, category } = req.body;
+    const handle = req.params.handle;
+
+    const toUser = await (isValidObjectId(handle)
+      ? User.findById(handle)
+      : User.findOne({ username: handle }));
+
+    if (!toUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (isAnonymous && !toUser.allowAnonymous) {
+      return res.status(400).json({
+        error: {
+          field: "isAnonymous",
+          message: "User does not allow anonymous questions",
+        },
+      });
+    }
+
+    if (category && !toUser.hasCategory(category)) {
+      return res.status(400).json({
+        error: {
+          field: "category",
+          message: `User does not have ${category} category`,
+        },
+      });
+    }
+
+    await Question.create({
+      fromUser: req.user!.id,
+      toUser: toUser.id,
+      question,
+      isAnonymous,
+      category,
+    });
+
+    return res.status(201).json({ message: "Question created successfully" });
+  } catch (e) {
+    const errors = mongooseErrors(e);
+    if (errors.length) {
+      return res.status(400).json({ errors });
+    }
+    next(e);
+  }
+}
