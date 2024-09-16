47d0f8ba90a31b353d309992df639a6fa4a4a557
Mahmoud Elnagar <m.elnagar@expressit.com.sa>
2024-09-16 14:48:47 +0300

    Remove Swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Askify API')
-    .setDescription('The Askify API description')
-    .setVersion('1.0')
-    .addTag('askify')
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
-
   const configService = app.get(ConfigService);
   await app.listen(configService.port);
   console.log(`Server is running on port ${configService.port}`);
